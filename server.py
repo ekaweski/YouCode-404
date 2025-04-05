780adca919c98245e3861396357f9a54b293a3d2
ekaweski <ellaewingk@gmail.com>
2025-04-05 14:14:42 -0700

    added Flask backend with SQLite database for users

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configure SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///website.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Define the User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# Route to get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
+
+# Route to create a new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'id': new_user.id, 'name': new_user.name, 'email': new_user.email}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
