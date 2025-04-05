bc71c14a736d3d72772f187ec9c96efe71d89b3a
ekaweski <ellaewingk@gmail.com>
2025-04-05 16:31:02 -0700

    IT WORKED!

--- app.py
+++ app.py
@@ -13,18 +13,18 @@ def login():
     password1 = request.form['password']
     user_choice1 = request.form['user_choice']
 
-    if email1 not in database:
+    if username1 not in database:
         return render_template('login.html',info='Invalid User')
     else:
 
-        if database[email1]!=password1:
+        if database[username1]!=password1:
             return render_template('login.html',info='Invalid Password')
         
         else:
-            if user_choice =='recipient':
+            if user_choice1 =='recipient':
                 return render_template('home_recipient.html',name=username1)
             else:
                 return render_template('home_donor.html',name=username1)
 
-if __name__ == 'main':
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
